a5667e2e45b398df7cee95e1d9616fbf7e7794d4
jksafe <jsafe7@gmail.com>
2023-07-30 22:34:25 -0400

    version 1.1+ of pixelate() complete
    pixelate() function generalized to accept filenames of images and videos as inputs and leverages class "cell" for core functionality and batching

---
+++ Projects/convert_IMG_VID.m
@@ -0,0 +1,16 @@
+function cell = convert_IMG_VID(filename)
+cell={};
+
+try
+    input_image = imread(filename);
+    cell{end+1} = input_image;
+catch
+    input_video = VideoReader(filename);
+    num_frames = floor(input_video.Duration)*input_video.FrameRate;
+    
+    for x=1:num_frames
+        cell{end+1} = read(input_video,x);
+    end
+end
+
+end
