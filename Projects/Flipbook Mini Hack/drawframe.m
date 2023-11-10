a94d9533a26fc84ec83380ccbb633cec5ce0594d
jksafe <jsafe7@gmail.com>
2023-11-10 13:38:26 -0500

    flipbook projs

---
+++ Projects/Flipbook Mini Hack/drawframe.m
@@ -0,0 +1,37 @@
+function drawframe(n)
+in=uint8(padarray(7*ones(100),[50 50],0,'both'));
+o=in;
+[rw,cl]=size(o);
+
+%{
+if n>24
+    n=25-(n-24);
+end
+%}
+
+for i=1:n
+    if n==1 
+        break;
+    else
+    for r=1:rw
+        for c=1:cl
+            if in(r,c)>3
+                o(r-1,c)=o(r-1,c)+1;
+                o(r+1,c)=o(r+1,c)+1;
+                o(r,c-1)=o(r,c-1)+1;
+                o(r,c+1)=o(r,c+1)+1;
+                o(r,c)=o(r,c)-4;
+            end
+        end
+    end
+    end
+    in=o;
+end
+
+cmp=colormap(slanCM('vivid',max(max(o))));
+o=ind2rgb(o,cmp);
+o=imresize(o,[1000 1000],'box');
+imshow(o)
+end
+
+
