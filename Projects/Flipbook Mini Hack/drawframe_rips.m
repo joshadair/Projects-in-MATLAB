bae812612f0deabbf4b508fee507292c5bae0354
jksafe <jsafe7@gmail.com>
2023-11-20 06:51:03 -0500

    flipbook
    updates

--- Projects/Flipbook Mini Hack/drawframe_rips.m
+++ Projects/Flipbook Mini Hack/drawframe_rips.m
@@ -1,14 +1,15 @@
-function drawframe_rips(f)
+function [ind,o]=drawframe_rips(f)
 
-tablesize=100;
-pebbles=5;
+ts=100;
+p=1;
 
-out=zeros(tablesize);
-timer=out;
+o=zeros(ts);
+tm=o;
 rng(1);
-locs_pebbles=randperm(tablesize^2,pebbles);
-out(locs_pebbles)=2;
-timer(locs_pebbles)=1;
+lcP=randperm(ts^2,p);
+o(lcP)=2;
+tm(lcP)=1;
+
 
 for i1=1:f   
 if f==1
@@ -16,36 +17,39 @@ if f==1
 end
 
 rng(1);
-new_peb=randi(tablesize^2);
+nP=randi(ts^2,round(i1/.75)+1);
 if i1==1
-out(new_peb)=2;
+o(nP)=2;
 end
-timer(new_peb)=i1;
+tm(nP)=i1;
 
-out=padarray(out,[f f],0,'both');
-timer=padarray(timer,[f f],0,'both');
-[rpebs,cpebs]=find(timer>0);
+o=padarray(o,[f f],0,'both');
+tm=padarray(tm,[f f],0,'both');
+[rP,cpebs]=find(tm>0);
 
-for i2=1:numel(rpebs)
-        t=timer(rpebs(i2),cpebs(i2));
+for i2=1:numel(rP)
+        t=tm(rP(i2),cpebs(i2));
         newrip=ones(2*t+1);
         newrip(2:end-1,2:end-1)=0;
-        out(rpebs(i2)-t:rpebs(i2)+t,cpebs(i2)-t:cpebs(i2)+t)=out(rpebs(i2)-t:rpebs(i2)+t,cpebs(i2)-t:cpebs(i2)+t)+newrip;
+        o(rP(i2)-t:rP(i2)+t,cpebs(i2)-t:cpebs(i2)+t)=o(rP(i2)-t:rP(i2)+t,cpebs(i2)-t:cpebs(i2)+t)+newrip;
         oldrip=ones(2*t-1);
         if t==1
-            out(rpebs(i2),cpebs(i2))=out(rpebs(i2),cpebs(i2))-2;
+            o(rP(i2),cpebs(i2))=o(rP(i2),cpebs(i2))-2;
         elseif t>1
             oldrip(2:end-1,2:end-1)=0;
-            out(rpebs(i2)-(t-1):rpebs(i2)+(t-1),cpebs(i2)-(t-1):cpebs(i2)+(t-1))=out(rpebs(i2)-(t-1):rpebs(i2)+(t-1),cpebs(i2)-(t-1):cpebs(i2)+(t-1))-oldrip;
+            o(rP(i2)-(t-1):rP(i2)+(t-1),cpebs(i2)-(t-1):cpebs(i2)+(t-1))=o(rP(i2)-(t-1):rP(i2)+(t-1),cpebs(i2)-(t-1):cpebs(i2)+(t-1))-oldrip;
         end 
 
-        timer(rpebs(i2),cpebs(i2))=t+1;
+        tm(rP(i2),cpebs(i2))=t+1;
 end
-out=out(f+1:end-f,f+1:end-f);
-timer=timer(f+1:end-f,f+1:end-f);
+o=o(f+1:end-f,f+1:end-f);
+tm=tm(f+1:end-f,f+1:end-f);
 end
-%cmp=colormap(turbo(max(max(timer))+1));
-%out=ind2rgb(out,cmp);
-imshow(out)
+cmp=colormap(turbo(max(max(o))));
+o=uint8(o);
+o=imresize(o,[1000 1000],'box');
+ind=o;
+o=ind2rgb(o,cmp);
+imshow(o)
 end
 
