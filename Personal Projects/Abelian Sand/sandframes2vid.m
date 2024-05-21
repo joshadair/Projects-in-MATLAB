ef69fd54ec47719120190edaf9acdecbe563dc61
jksafe <jsafe7@gmail.com>
2024-05-21 13:53:27 -0400

    05212024
    clean2

--- Personal Projects/Abelian Sand/sandframes2vid.m
+++ Personal Projects/Abelian Sand/sandframes2vid.m
@@ -1,25 +1,31 @@
 function average=sandframes2vid(frames,edgeCondition,filename,duration)
-%set(0,'DefaultFigureVisible','off');
+
 if strcmp(edgeCondition,'grow')==1
     frames=normalizesandsize(frames);
 end
 
-video=VideoWriter(filename,'Indexed AVI');
+frames=frames+1;
+
+v=VideoWriter(filename,'Indexed AVI');
 framerate=floor(length(frames)/duration);
-video.FrameRate=framerate;
+v.FrameRate=framerate;
 
 cmapList={'Parula','Turbo','HSV','Hot','Cool','Winter','Spring','Summer','Autumn','Sky','Abyss'};
 [indx,~]=listdlg('PromptString',"Please select a colormap from the list below...",'ListString',cmapList,'SelectionMode','single');
-
-%numCol=max(max(max(frames)))-1;
 numCol=max(max(max(frames)))+1;
-%cmapName=cat(2,cmapList{indx},'(',num2str(numCol),')');
-%cmap=colormap(cmapName);
-cmap=slanCM('turbo',numCol);
-%cmap(1,:)=[0 0 0];
-%cmap(end+1,:)=[1 1 1];
-video.Colormap=cmap;
-%{
+cmapName=cat(2,cmapList{indx},'(',num2str(numCol),')');
+cmap=colormap(cmapName);
+v.Colormap=cmap;
+
+average=zeros(size(frames,2));
+for i1=1:size(frames,3)
+    temp=ind2rgb(frames(:,:,i1),cmap);
+    average=average+temp;
+end
+average=average/size(frames,3);
+average=imresize(average,[1080 1080],'box');
+imshow(average)
+
 % Pad beginning of video with additional frames of initial setup
 for i1=1:ceil(length(frames)*.04)
     frames=cat(3,frames(:,:,1),frames);
@@ -29,25 +35,10 @@ end
 for i1=1:ceil(length(frames)*.08)
     frames=cat(3,frames,frames(:,:,end));
 end
-%}
+
 frames=imresize(frames,[540 540],'box');
-[row,col]=size(frames(:,:,1));
-average=zeros(row,col,3);
-nFrames=size(frames,3);
 
-for i1=1:nFrames
-    temp=ind2rgb(frames(:,:,i1),cmap);
-    average=average+temp;
-    %writeVideo(video,temp);
-end
-
-average=average/nFrames;
-average=imresize(average,[1080 1080],'box');
-imshow(average)
-
-
-
-open(video);
-writeVideo(video,frames);
-close(video);
+open(v);
+writeVideo(v,frames);
+close(v);
 end
---
+++ Personal Projects/Abelian Sand/sandplusdecay.m
@@ -0,0 +1,252 @@
+function frames=sandplusdecay(initial,edgecondition)
+
+if strcmp(edgecondition,'falloff')==1
+    edge=1;
+    frames=[];
+    frames(:,:,1)=initial;
+elseif strcmp(edgecondition,'grow')==1
+    edge=2;
+    frames={};
+    frames{end+1}=initial;
+end
+[row,col]=size(initial);
+new=initial;
+
+while max(max(new))>11
+    initial=new;
+
+    if edge==2
+        edges=[initial(1,:),initial(end,:),initial(:,1)',initial(:,end)'];
+        if max(edges)>3
+            initial=padarray(initial,[1 1]);
+            new=initial;
+        end
+    end
+
+    % If using falloff condition for edges of table, need to
+    % account for critical points at pile corners and edges
+    if edge==1
+        for r=1:row
+            for c=1:col
+                if initial(r,c)>11
+                    if r==1 && c==1
+                        new(r,c+1)=new(r,c+1)+2;
+                        new(r+1,c)=new(r+1,c)+2;
+                        new(r,c+2)=new(r,c+2)+1;
+                        new(r+2,c)=new(r+2,c)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif r==1 && c==2
+                        new(r,c-1)=new(r,c-1)+2;
+                        new(r,c+1)=new(r,c+1)+2;
+                        new(r+1,c)=new(r+1,c)+2;
+                        new(r,c+2)=new(r,c+2)+1;
+                        new(r+2,c)=new(r+2,c)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif r==1 && c==col-1
+                        new(r,c-1)=new(r,c-1)+2;
+                        new(r,c+1)=new(r,c+1)+2;
+                        new(r+1,c)=new(r+1,c)+2;
+                        new(r,c-2)=new(r,c-2)+1;
+                        new(r+2,c)=new(r+2,c)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif r==1 && c==col
+                        new(r,c-1)=new(r,c-1)+2;
+                        new(r+1,c)=new(r+1,c)+2;
+                        new(r,c-2)=new(r,c-2)+1;
+                        new(r+2,c)=new(r+2,c)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif r==2 && c==1
+                        new(r-1,c)=new(r-1,c)+2;
+                        new(r,c+1)=new(r,c+1)+2;
+                        new(r+1,c)=new(r+1,c)+2;
+                        new(r,c+2)=new(r,c+2)+1;
+                        new(r+2,c)=new(r+2,c)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif r==2 && c==2
+                        new(r-1,c)=new(r-1,c)+2;
+                        new(r,c-1)=new(r,c-1)+2;
+                        new(r,c+1)=new(r,c+1)+2;
+                        new(r+1,c)=new(r+1,c)+2;
+                        new(r,c+2)=new(r,c+2)+1;
+                        new(r+2,c)=new(r+2,c)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif r==2 && c==col-1
+                        new(r-1,c)=new(r-1,c)+2;
+                        new(r,c-1)=new(r,c-1)+2;
+                        new(r,c+1)=new(r,c+1)+2;
+                        new(r+1,c)=new(r+1,c)+2;
+                        new(r,c-2)=new(r,c-2)+1;
+                        new(r+2,c)=new(r+2,c)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif r==2 && c==col
+                        new(r-1,c)=new(r-1,c)+2;
+                        new(r,c-1)=new(r,c-1)+2;
+                        new(r+1,c)=new(r+1,c)+2;
+                        new(r,c-2)=new(r,c-2)+1;
+                        new(r+2,c)=new(r+2,c)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif r==row-1 && c==1
+                        new(r-1,c)=new(r-1,c)+2;
+                        new(r,c+1)=new(r,c+1)+2;
+                        new(r+1,c)=new(r+1,c)+2;
+                        new(r-2,c)=new(r-2,c)+1;
+                        new(r,c+2)=new(r,c+2)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif r==row-1 && c==2
+                        new(r-1,c)=new(r-1,c)+2;
+                        new(r,c-1)=new(r,c-1)+2;
+                        new(r,c+1)=new(r,c+1)+2;
+                        new(r+1,c)=new(r+1,c)+2;
+                        new(r-2,c)=new(r-2,c)+1;
+                        new(r,c+2)=new(r,c+2)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif r==row-1 && c==col-1
+                        new(r-1,c)=new(r-1,c)+2;
+                        new(r,c-1)=new(r,c-1)+2;
+                        new(r,c+1)=new(r,c+1)+2;
+                        new(r+1,c)=new(r+1,c)+2;
+                        new(r-2,c)=new(r-2,c)+1;
+                        new(r,c-2)=new(r,c-2)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif r==row-1 && c==col
+                        new(r-1,c)=new(r-1,c)+2;
+                        new(r,c-1)=new(r,c-1)+2;
+                        new(r+1,c)=new(r+1,c)+2;
+                        new(r-2,c)=new(r-2,c)+1;
+                        new(r,c-2)=new(r,c-2)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif r==row && c==1
+                        new(r-1,c)=new(r-1,c)+2;
+                        new(r,c+1)=new(r,c+1)+2;
+                        new(r-2,c)=new(r-2,c)+1;
+                        new(r,c+2)=new(r,c+2)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif r==row && c==2
+                        new(r-1,c)=new(r-1,c)+2;
+                        new(r,c-1)=new(r,c-1)+2;
+                        new(r,c+1)=new(r,c+1)+2;
+                        new(r-2,c)=new(r-2,c)+1;
+                        new(r,c+2)=new(r,c+2)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif r==row && c==col-1
+                        new(r-1,c)=new(r-1,c)+2;
+                        new(r,c-1)=new(r,c-1)+2;
+                        new(r,c+1)=new(r,c+1)+2;
+                        new(r-2,c)=new(r-2,c)+1;
+                        new(r,c-2)=new(r,c-2)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif r==row && c==col
+                        new(r-1,c)=new(r-1,c)+2;
+                        new(r,c-1)=new(r,c-1)+2;
+                        new(r-2,c)=new(r-2,c)+1;
+                        new(r,c-2)=new(r,c-2)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif r==1
+                        new(r,c-1)=new(r,c-1)+2;
+                        new(r,c+1)=new(r,c+1)+2;
+                        new(r+1,c)=new(r+1,c)+2;
+                        new(r,c-2)=new(r,c-2)+1;
+                        new(r,c+2)=new(r,c+2)+1;
+                        new(r+2,c)=new(r+2,c)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif r==2
+                        new(r-1,c)=new(r-1,c)+2;
+                        new(r,c-1)=new(r,c-1)+2;
+                        new(r,c+1)=new(r,c+1)+2;
+                        new(r+1,c)=new(r+1,c)+2;
+                        new(r,c-2)=new(r,c-2)+1;
+                        new(r,c+2)=new(r,c+2)+1;
+                        new(r+2,c)=new(r+2,c)+1;
+                        new(r,c)=new(r,c)-12;
+
+                    elseif r==row-1
+                        new(r-1,c)=new(r-1,c)+2;
+                        new(r,c-1)=new(r,c-1)+2;
+                        new(r,c+1)=new(r,c+1)+2;
+                        new(r+1,c)=new(r+1,c)+2;
+                        new(r-2,c)=new(r-2,c)+1;
+                        new(r,c-2)=new(r,c-2)+1;
+                        new(r,c+2)=new(r,c+2)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif r==row
+                        new(r-1,c)=new(r-1,c)+2;
+                        new(r,c-1)=new(r,c-1)+2;
+                        new(r,c+1)=new(r,c+1)+2;
+                        new(r-2,c)=new(r-2,c)+1;
+                        new(r,c-2)=new(r,c-2)+1;
+                        new(r,c+2)=new(r,c+2)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif c==1
+                        new(r-1,c)=new(r-1,c)+2;
+                        new(r,c+1)=new(r,c+1)+2;
+                        new(r+1,c)=new(r+1,c)+2;
+                        new(r-2,c)=new(r-2,c)+1;
+                        new(r,c+2)=new(r,c+2)+1;
+                        new(r+2,c)=new(r+2,c)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif c==2
+                        new(r-1,c)=new(r-1,c)+2;
+                        new(r,c-1)=new(r,c-1)+2;
+                        new(r,c+1)=new(r,c+1)+2;
+                        new(r+1,c)=new(r+1,c)+2;
+                        new(r-2,c)=new(r-2,c)+1;
+                        new(r,c+2)=new(r,c+2)+1;
+                        new(r+2,c)=new(r+2,c)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif c==col-1
+                        new(r-1,c)=new(r-1,c)+2;
+                        new(r,c-1)=new(r,c-1)+2;
+                        new(r,c+1)=new(r,c+1)+2;
+                        new(r+1,c)=new(r+1,c)+2;
+                        new(r-2,c)=new(r-2,c)+1;
+                        new(r,c-2)=new(r,c-2)+1;
+                        new(r+2,c)=new(r+2,c)+1;
+                        new(r,c)=new(r,c)-12;
+                    elseif c==col
+                        new(r-1,c)=new(r-1,c)+2;
+                        new(r,c-1)=new(r,c-1)+2;
+                        new(r+1,c)=new(r+1,c)+2;
+                        new(r-2,c)=new(r-2,c)+1;
+                        new(r,c-2)=new(r,c-2)+1;
+                        new(r+2,c)=new(r+2,c)+1;
+                        new(r,c)=new(r,c)-12;
+                    else
+                        new(r-1,c)=new(r-1,c)+2;
+                        new(r,c-1)=new(r,c-1)+2;
+                        new(r,c+1)=new(r,c+1)+2;
+                        new(r+1,c)=new(r+1,c)+2;
+                        new(r-2,c)=new(r-2,c)+1;
+                        new(r,c-2)=new(r,c-2)+1;
+                        new(r,c+2)=new(r,c+2)+1;
+                        new(r+2,c)=new(r+2,c)+1;
+                        new(r,c)=new(r,c)-12;
+                    end
+                end
+            end
+        end
+        frames=cat(3,frames,new);
+    end
+
+    if edge==2
+        [row,col]=size(initial);
+        for r=1:row
+            for c=1:col
+                if initial(r,c)>11
+                    new(r-1,c)=new(r-1,c)+2;
+                    new(r,c-1)=new(r,c-1)+2;
+                    new(r,c+1)=new(r,c+1)+2;
+                    new(r+1,c)=new(r+1,c)+2;
+                    new(r-2,c)=new(r-2,c)+1;
+                    new(r,c-2)=new(r,c-2)+1;
+                    new(r,c+2)=new(r,c+2)+1;
+                    new(r+2,c)=new(r+2,c)+1;
+                    new(r,c)=new(r,c)-12;
+                end
+            end
+        end
+        frames{end+1}=new;
+    end
+
+end
+
+end
